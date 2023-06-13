52113631d2efd49bcdb55eae1a3f4f423fe0ebb7
Erikas Raščius <erikas@flairdigital.co>
2023-06-13 17:47:09 +0300

    chore: remove table heading

--- src/components/HeroTable.jsx
+++ src/components/HeroTable.jsx
@@ -1,27 +1,27 @@
-import React from 'react';
-import Paper from '@mui/material/Paper';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import {getHeroTableData} from '../api/CoinGecko';
-import '../css/RainbowText.css';
+import React from "react";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import { getHeroTableData } from "../api/CoinGecko";
+import "../css/RainbowText.css";
 const styles = {
   tableContainer: {
-    maxWidth: '100%',
-    marginLeft: 'auto',
-    marginRight: 'auto',
+    maxWidth: "100%",
+    marginLeft: "auto",
+    marginRight: "auto",
   },
   table: {
-    width: '100%',
+    width: "100%",
   },
 };
 function createData(title, value) {
-  return {title, value};
+  return { title, value };
 }
-const numberFormatter = new Intl.NumberFormat('en-US');
+const numberFormatter = new Intl.NumberFormat("en-US");
 export default function HeroTable() {
   const [data, setData] = React.useState({});
   const [rows, setRows] = React.useState([]);
@@ -30,38 +30,44 @@ export default function HeroTable() {
       const d = await getHeroTableData();
       setData(d);
       setRows([
-        createData('Total Supply', numberFormatter.format(d.totalSupply)),
-        createData('Circulating Supply', numberFormatter.format(d.circulatingSupply)),
-        createData('Burned', numberFormatter.format(d.burned)),
-        createData('Market Cap', `$ ${numberFormatter.format(d.marketCap)}`),
-        createData('Fully Diluted Market Cap', `$ ${numberFormatter.format(d.fullyDilutedMarketCap)}`),
-        createData('Price', `$ ${d.price.toFixed(11)}`),
+        createData("Total Supply", numberFormatter.format(d.totalSupply)),
+        createData(
+          "Circulating Supply",
+          numberFormatter.format(d.circulatingSupply)
+        ),
+        createData("Burned", numberFormatter.format(d.burned)),
+        createData("Market Cap", `$ ${numberFormatter.format(d.marketCap)}`),
+        createData(
+          "Fully Diluted Market Cap",
+          `$ ${numberFormatter.format(d.fullyDilutedMarketCap)}`
+        ),
+        createData("Price", `$ ${d.price.toFixed(11)}`),
       ]);
     }
     e();
   }, []);
   return (
-      <TableContainer component={Paper} sx={styles.tableContainer}>
-        <Table sx={styles.table} aria-label="simple table">
-          <TableHead>
+    <TableContainer component={Paper} sx={styles.tableContainer}>
+      <Table sx={styles.table} aria-label="simple table">
+        {/* <TableHead>
             <TableRow>
               <TableCell>Title</TableCell>
               <TableCell align="right">Value</TableCell>
             </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.map((row) => (
-                <TableRow key={row.name}>
-                  <TableCell component="th" scope="row">
-                    {row.title}
-                  </TableCell>
-                  <TableCell align="right" className="tableText">
-                    <b>{row.value}</b>
-                  </TableCell>
-                </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+          </TableHead> */}
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.name}>
+              <TableCell component="th" scope="row">
+                {row.title}
+              </TableCell>
+              <TableCell align="right" className="tableText">
+                <b>{row.value}</b>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
